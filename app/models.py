26eab8d2c2ac1adfec77404a3618fc0b3e9fad60
anubhav <anubhavsaxenavibhu@gmail.com>
2025-06-07 00:35:57 +0530

    initial commit

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, Float, Integer, DateTime
 from .database import Base
-
+import os
 class Trade(Base):
     __tablename__ = "trades"
     id = Column(Integer, primary_key=True, index=True)
