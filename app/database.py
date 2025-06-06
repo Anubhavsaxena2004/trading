aa6deca9d02bb9ba927a7e52516a060580431ad7
anubhav <anubhavsaxenavibhu@gmail.com>
2025-06-07 00:37:50 +0530

    initial commit

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
+import os,matplotlib
 
 # Load environment variables
 from dotenv import load_dotenv
