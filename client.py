54a1581362adc6411f2f4ba26b1d6e44fd5fe629
anubhav <anubhavsaxenavibhu@gmail.com>
2025-06-12 07:52:36 +0530

    initial commit

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from collections import deque
 from tasks import notify_threshold
 import requests
 from datetime import datetime
-import os,math,matplotlib,pandas
+import os,math,matplotlib,pandas,numpy
 
 PRICE_HISTORY = {}  # {ticker: deque(maxlen=60)}
 AVG_PRICES = {}  # {ticker: [prices]}
